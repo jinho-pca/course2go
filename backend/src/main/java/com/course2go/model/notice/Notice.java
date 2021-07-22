71e08763f5ca5899b32d25a191bdcf263e8d60b0
nhs <sjsjsjghkdwp@naver.com>
2021-07-22 15:25:38 +0900

    feat: Make noticeDto unfinished
    NoticeDto 생성, 기초공사

---
+++ backend/src/main/java/com/course2go/model/notice/Notice.java
@@ -0,0 +1,31 @@
+package com.course2go.model.notice;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonInclude;
+
+import lombok.Data;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class Notice{
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int nid;
+
+    private int noticeUid;
+    private int noticeType;
+    private int noticeFromUid;
+    private int noticeNnid;
+    private boolean noticeIsnew;
+
+}
