c5ab3fe151112903bda520df79465864460fce42
nhs <sjsjsjghkdwp@naver.com>
2021-07-22 15:32:25 +0900

    feat: Make noticeController unfinished
    noticeController 생성

---
+++ backend/src/main/java/com/course2go/controller/NoticeController.java
@@ -0,0 +1,10 @@
+package com.course2go.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+import com.course2go.model.notice.Notice;
+
+@RestController
+public class NoticeController {
+	
+}
