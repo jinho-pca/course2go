db3c257765e97baee007ae437ce2a5517245987c
jinho_pca <jinho_pca@naver.com>
2021-07-26 17:21:07 +0900

    feat: Update request from, profile service
    요청 포맷용 파일, 프로필보기 기능 구현

---
+++ backend/src/main/java/com/course2go/model/user/UserModifyRequest.java
@@ -0,0 +1,35 @@
+package com.course2go.model.user;
+
+public class UserModifyRequest {
+	
+	String userEmail;
+	
+	String userNickname;
+	
+	String userPassword;
+
+	public String getUserEmail() {
+		return userEmail;
+	}
+
+	public void setUserEmail(String userEmail) {
+		this.userEmail = userEmail;
+	}
+
+	public String getUserNickname() {
+		return userNickname;
+	}
+
+	public void setUserNickname(String userNickname) {
+		this.userNickname = userNickname;
+	}
+
+	public String getUserPassword() {
+		return userPassword;
+	}
+
+	public void setUserPassword(String userPassword) {
+		this.userPassword = userPassword;
+	}
+	
+}
---
+++ backend/src/main/java/com/course2go/service/user/UserProfileService.java
@@ -0,0 +1,5 @@
+package com.course2go.service.user;
+
+public interface UserProfileService {
+	public Object userProfileShow(String userNickname);
+}
