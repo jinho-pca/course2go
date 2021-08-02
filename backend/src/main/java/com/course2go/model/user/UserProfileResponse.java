d591a989167bc2f86be8b2e49a6f607ccfa4e967
jinho_pca <jinho_pca@naver.com>
2021-08-03 01:22:59 +0900

    feat: Update backend-user part using JWT
    JWT 토큰 사용으로 인한 코드 수정 및 Dto 분리

---
+++ backend/src/main/java/com/course2go/model/user/UserProfileResponse.java
@@ -0,0 +1,44 @@
+package com.course2go.model.user;
+
+public class UserProfileResponse {
+
+    String userName;
+
+    String userNickname;
+
+    String userImage;
+
+    String userComment;
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getUserNickname() {
+        return userNickname;
+    }
+
+    public void setUserNickname(String userNickname) {
+        this.userNickname = userNickname;
+    }
+
+    public String getUserImage() {
+        return userImage;
+    }
+
+    public void setUserImage(String userImage) {
+        this.userImage = userImage;
+    }
+
+    public String getUserComment() {
+        return userComment;
+    }
+
+    public void setUserComment(String userComment) {
+        this.userComment = userComment;
+    }
+}
--- backend/src/main/java/com/course2go/service/user/UserProfileModifyService.java
+++ backend/src/main/java/com/course2go/service/user/UserProfileModifyService.java
@@ -5,5 +5,5 @@ import com.course2go.model.user.User;
 public interface UserProfileModifyService {
 	
 	// 프로필 수정
-	public int userProfileModify(User user);
+	public int userProfileModify(String tokenEmail, String requestComment, String imageUrl);
 }
