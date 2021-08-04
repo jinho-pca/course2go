f2569bc5c29317e0cf1448ede0539be52686311c
nhs <sjsjsjghkdwp@naver.com>
2021-08-04 11:16:01 +0900

    bug: Fix bugs
    루트 Dao 내 쓸모없는 매서드 삭제, 스크랩 임플리먼트 연결

--- backend/src/main/java/com/course2go/service/scrap/ScrapServicaImpl.java
+++ backend/src/main/java/com/course2go/service/scrap/ScrapServicaImpl.java
@@ -3,6 +3,18 @@ package com.course2go.service.scrap;
 import org.springframework.stereotype.Service;
 
 @Service
-public class ScrapServicaImpl {
+public class ScrapServicaImpl implements ScrapService {
+
+	@Override
+	public boolean scrap(String uid, Integer bid) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean cancelScrap(String uid, Integer bid) {
+		// TODO Auto-generated method stub
+		return false;
+	}
 
 }
