3d82c05f9ca6422501b65397c6a5d681175f023b
jinho_pca <jinho_pca@naver.com>
2021-08-05 22:55:43 +0900

    feat: Update show user profile
    프로필보기 시 팔로잉, 팔로워 수 포함, header 사용, 본인프로필/타인프로필 구분
    
    Resolved: S05P13A106-47

--- backend/src/main/java/com/course2go/service/follow/FollowListService.java
+++ backend/src/main/java/com/course2go/service/follow/FollowListService.java
@@ -8,5 +8,7 @@ public interface FollowListService {
 	public List<String> getFollowingListByUid(String uid);
 	public List<String> getFollowerList(String email);
 	public List<String> getFollowingList(String email);
+	public int getFollowerCount(String userNickname);
+	public int getFollowingCount(String userNickname);
 	
 }
