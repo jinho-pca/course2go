f5f4eb46951e98f90073609baeb8b19ec4a6c53d
jinho_pca <jinho_pca@naver.com>
2021-08-05 10:15:34 +0900

    feat: Update WebConfig
    CORS 오류로 인해 WebConfig 수정

--- backend/src/main/java/com/course2go/config/WebConfig.java
+++ backend/src/main/java/com/course2go/config/WebConfig.java
@@ -9,7 +9,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry){
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:8080")
+                .allowedOrigins("http://13.209.13.47:8080")
                 .allowCredentials(true);
     }
 }
