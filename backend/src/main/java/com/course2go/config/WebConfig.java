98bc93f8fcc33f25af71dce28d41666355089aca
jinho_pca <jinho_pca@naver.com>
2021-08-05 11:34:47 +0900

    feat: Update WebConfig
    CORS 문제로 인한 front에서 jwt 읽기 안되는 문제 수정

--- backend/src/main/java/com/course2go/config/WebConfig.java
+++ backend/src/main/java/com/course2go/config/WebConfig.java
@@ -10,6 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry){
         registry.addMapping("/**")
                 .allowedOrigins("http://localhost:8080/")
+                .exposedHeaders("Authorization")
                 .allowCredentials(true);
     }
 }
