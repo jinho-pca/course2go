8795d7fcda75d1d0aaf2c0cab7b1a76fc338f899
nhs <sjsjsjghkdwp@naver.com>
2021-08-10 16:41:36 +0900

    feat: Add information in CommentDto
    댓글부분 요구사항 변경에 의해 유저 프로필사진 이미지가 필요해져 commentDto에 UserDto를 담기로 결정하여 변경

--- backend/src/main/java/com/course2go/model/user/UserDto.java
+++ backend/src/main/java/com/course2go/model/user/UserDto.java
@@ -1,5 +1,10 @@
 package com.course2go.model.user;
 
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
 public class UserDto {
 
     String userName;
