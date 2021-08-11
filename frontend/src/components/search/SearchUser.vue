4b1a75804847cb93bec5f6db5ccf53fb9152a5ea
안기훈 <dksdmlwjd1@naver.com>
2021-08-11 15:41:50 +0900

    feat: 회원 검색 기능 및 검색 디자인
    composition API emit, props를 이용한 회원 검색

---
+++ frontend/src/components/search/SearchUser.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <SearchUserInput @getWord="getWord"/>
+    <SearchUserList v-for="(user, idx) in users" :key="idx" :user="user" />
+  </div>
+</template>
+
+<script>
+import SearchUserInput from '@/components/search/SearchUserInput.vue'
+import SearchUserList from '@/components/search/SearchUserList.vue'
+import { search } from '@/compositions/search.js'
+
+export default {
+  name: 'SearchUser',
+  components: {
+    SearchUserInput,
+    SearchUserList
+  },
+  setup() {
+    const { searchUser, users } = search()
+    async function getWord(word) {
+      searchUser(word)
+    }
+    return { getWord, users }
+  }
+}
+</script>
--- frontend/src/compositions/search.js
+++ frontend/src/compositions/search.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
+import { ref } from 'vue';
 import { BASE_URL } from '@/compositions/global.js'
 
-const { URL } = BASE_URL();
+const { URL, token } = BASE_URL();
 
 export const searchPlace = async (key, page) => {
 
@@ -21,4 +22,54 @@ export const searchPlace = async (key, page) => {
 		return err
 	})
 	return result;
+}
+
+export const search = () => {
+
+	const page = ref(1)
+
+	const onClickSelect = (e) => {
+    const isActive = e.currentTarget.className.indexOf("active") !== -1;
+    if (isActive) {
+      e.currentTarget.className = "search-select";
+    } else {
+      e.currentTarget.className = "search-select active";
+    }
+  }
+
+	const onClickOption = (e) => {
+    const selectedValue = e.currentTarget.innerHTML;
+    document.querySelector(".search-tab .search-text").innerHTML = selectedValue;
+		if (e.currentTarget.value == 1 && page.value == 2) {
+			page.value = 1
+		} else if (e.currentTarget.value == 2 && page.value == 1) {
+			page.value = 2
+		}
+  }
+
+	const users = ref([])
+
+	const searchUser = (word) => {
+		axios({
+			method: 'get',
+			url: URL + 'search/user/' + word,
+      headers: {
+        Authorization: token,
+      }
+		})
+		.then((res) => {
+			console.log(res.data)
+			users.value = res.data
+			return res
+		})
+		.catch((err) => {
+			console.log(err)
+		})
+	}
+
+	// const searchRoute = (word) => {
+	// 	continue
+	// }
+
+	return { onClickSelect, onClickOption, page, searchUser, users }
 }
