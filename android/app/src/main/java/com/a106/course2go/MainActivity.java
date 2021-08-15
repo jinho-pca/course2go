74e36e44cd6fe478796a74dded84f1b88b51bf9c
안기훈 <dksdmlwjd1@naver.com>
2021-08-15 17:21:11 +0900

    feat: Update android webview app
    안드로이드 웹뷰 앱 추가

---
+++ android/app/src/main/java/com/a106/course2go/MainActivity.java
@@ -0,0 +1,26 @@
+package com.a106.course2go;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends AppCompatActivity {
+    WebView webView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        webView = findViewById(R.id.webView);
+        webView.setWebViewClient(new WebViewClient());
+
+        WebSettings webSettings = webView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        webView.getSettings().setDomStorageEnabled(true);
+
+        webView.loadUrl("https://i5a106.p.ssafy.io");
+    }
+}
