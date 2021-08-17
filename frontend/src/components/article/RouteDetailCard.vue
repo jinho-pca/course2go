d8ff8ff7498984c45e0e42fc1abe0f21fa656c7b
jinho_pca <jinho_pca@naver.com>
2021-08-17 11:53:06 +0900

    fix: Update frontend
    페이지로고수정(favicon.ico), 동선상세보기시간가격표기수정(RouteDetailCard.vue), 뉴스피드상단연필마크제거(NavTop.vue), 프로필페이지좋아요대신게시물로수정(ProfileCard.vue), 로그아웃confirm, alert추가(logout.js)
    
    Resolved: S05P13A106-94, S05P13A106-92

--- frontend/src/components/article/RouteDetailCard.vue
+++ frontend/src/components/article/RouteDetailCard.vue
@@ -2,21 +2,26 @@
   <div>
     <details open>
       <summary><span class="centersummary">세부사항</span></summary>
-      <hr>
+      <hr />
       <div class="content">
-        <span id="date">기간</span> <br>
-        {{articleRoute.routeResponse.routeStartDate}} ~ {{articleRoute.routeResponse.routeEndDate}}<br><br>
+        <span id="date">기간</span> <br />
+        {{ articleRoute.routeResponse.routeStartDate }} ~
+        {{ articleRoute.routeResponse.routeEndDate }}<br /><br />
         <span>동선</span>
-        <RouteMap :containSpots="articleRoute.containSpots"/>
+        <RouteMap :containSpots="articleRoute.containSpots" />
         <div v-for="(spot, index) in articleRoute.containSpots" :key="index">
-          <details v-if="spot.visit==null">
-            <summary class="novisitsummery"><span>{{spot.place.placeName}}</span></summary>
+          <details v-if="spot.visit == null">
+            <summary class="novisitsummery"
+              ><span>{{ spot.place.placeName }}</span></summary
+            >
           </details>
           <details v-else>
-            <summary><span class="centersummary">{{spot.place.placeName}}</span></summary>
-            <hr>
-            {{spot.visit.visitCost}}<br>
-            {{spot.visit.visitTime}}<br>
+            <summary
+              ><span class="centersummary">{{ spot.place.placeName }}</span></summary
+            >
+            <hr />
+            소요비용 : {{ spot.visit.visitCost }}원<br />
+            소요시간 : {{ spot.visit.visitTime }}시간<br />
             <span v-on:click="goVisit(index)">글 보러가기</span>
           </details>
         </div>
@@ -26,102 +31,102 @@
 </template>
 
 <script>
-import '../css/article/route-detail.css'
-import RouteMap from '@/components/maps/routemap/RouteMap.vue'
+import "../css/article/route-detail.css";
+import RouteMap from "@/components/maps/routemap/RouteMap.vue";
 export default {
-    name: 'routedetail',
-    props:{
-      articleRoute:{
-        type: Object
-      },
+  name: "routedetail",
+  props: {
+    articleRoute: {
+      type: Object,
     },
-    // created() {
-    //   console.log("카드")
-    //   console.log(this.routeReadResponse)
-    //   console.log("카드")
-    // },
-    components: {
-      RouteMap
+  },
+  // created() {
+  //   console.log("카드")
+  //   console.log(this.routeReadResponse)
+  //   console.log("카드")
+  // },
+  components: {
+    RouteMap,
+  },
+  data() {
+    return {
+      // routeReadResponse: {
+      //   routeResponse: {
+      //     routeStartDate: "2021-07-24",
+      //     routeEndDate: "2021-07-24",
+      //     routeContent: "우와여행너무재밌다",
+      //   },
+      //   boardResponse: {
+      //     boardTitle: "즐거운 제주도 여행",
+      //     boardLike: 100,
+      //     boardStar: 50,
+      //     boardTid: 1,
+      //     boardType: true,
+      //     boardTime: "2021-07-24 17:00:00",
+      //   },
+      //   containSpots: [
+      //     {
+      //       place: {
+      //         pid: 3,
+      //         placeName: "동방파제",
+      //         placeAddress: "제주특별자치도 서귀포시 서귀동 758-2",
+      //         placeLat: 33.236546,
+      //         placeLng: 126.568803,
+      //         placeDataDate: "2015-12-31",
+      //         placeType: "교통시설",
+      //       },
+      //       visit: {
+      //         vid: 3,
+      //         visitPid: 3,
+      //         visitContent: "동방파제 재밌당",
+      //         visitTime: 2,
+      //         visitCost: 10000,
+      //         visitImage1: "",
+      //         visitImage2: null,
+      //         visitImage3: null,
+      //       },
+      //     },
+      //     {
+      //       place: {
+      //         pid: 4,
+      //         placeName: "새섬",
+      //         placeAddress: "제주특별자치도 서귀포시 서귀동 산 3-3",
+      //         placeLat: 33.235071,
+      //         placeLng: 126.562554,
+      //         placeDataDate: "2015-12-31",
+      //         placeType: "지명관련",
+      //       },
+      //       visit: {
+      //         vid: 4,
+      //         visitPid: 4,
+      //         visitContent: "",
+      //         visitTime: 1,
+      //         visitCost: 50000,
+      //         visitImage1: "",
+      //         visitImage2: null,
+      //         visitImage3: null,
+      //       },
+      //     },
+      //     {
+      //       place: {
+      //         pid: 5,
+      //         placeName: "섶섬",
+      //         placeAddress: "제주특별자치도 서귀포시 보목동 산 1",
+      //         placeLat: 33.230314,
+      //         placeLng: 126.599675,
+      //         placeDataDate: "2015-12-31",
+      //         placeType: "지명관련",
+      //       },
+      //       visit: null,
+      //     }
+      //   ],
+      // },
+    };
+  },
+  methods: {
+    goVisit(index) {
+      console.log(index);
     },
-    data() {
-      return {
-        // routeReadResponse: {
-        //   routeResponse: {
-        //     routeStartDate: "2021-07-24",
-        //     routeEndDate: "2021-07-24",
-        //     routeContent: "우와여행너무재밌다",
-        //   },
-        //   boardResponse: {
-        //     boardTitle: "즐거운 제주도 여행",
-        //     boardLike: 100,
-        //     boardStar: 50,
-        //     boardTid: 1,
-        //     boardType: true,
-        //     boardTime: "2021-07-24 17:00:00",
-        //   },
-        //   containSpots: [
-        //     {
-        //       place: {
-        //         pid: 3,
-        //         placeName: "동방파제",
-        //         placeAddress: "제주특별자치도 서귀포시 서귀동 758-2",
-        //         placeLat: 33.236546,
-        //         placeLng: 126.568803,
-        //         placeDataDate: "2015-12-31",
-        //         placeType: "교통시설",
-        //       },
-        //       visit: {
-        //         vid: 3,
-        //         visitPid: 3,
-        //         visitContent: "동방파제 재밌당",
-        //         visitTime: 2,
-        //         visitCost: 10000,
-        //         visitImage1: "",
-        //         visitImage2: null,
-        //         visitImage3: null,
-        //       },
-        //     },
-        //     {
-        //       place: {
-        //         pid: 4,
-        //         placeName: "새섬",
-        //         placeAddress: "제주특별자치도 서귀포시 서귀동 산 3-3",
-        //         placeLat: 33.235071,
-        //         placeLng: 126.562554,
-        //         placeDataDate: "2015-12-31",
-        //         placeType: "지명관련",
-        //       },
-        //       visit: {
-        //         vid: 4,
-        //         visitPid: 4,
-        //         visitContent: "",
-        //         visitTime: 1,
-        //         visitCost: 50000,
-        //         visitImage1: "",
-        //         visitImage2: null,
-        //         visitImage3: null,
-        //       },
-        //     },
-        //     {
-        //       place: {
-        //         pid: 5,
-        //         placeName: "섶섬",
-        //         placeAddress: "제주특별자치도 서귀포시 보목동 산 1",
-        //         placeLat: 33.230314,
-        //         placeLng: 126.599675,
-        //         placeDataDate: "2015-12-31",
-        //         placeType: "지명관련",
-        //       },
-        //       visit: null,
-        //     }
-        //   ],
-        // },
-      }
-    },
-    methods: {
-      goVisit(index) {
-        console.log(index);
-      },
-    },
-}
+  },
+};
 </script>
--- frontend/src/components/profile/ProfileCard.vue
+++ frontend/src/components/profile/ProfileCard.vue
@@ -2,29 +2,52 @@
   <div class="profile-card">
     <div class="profile-top">
       <div class="profile-imagebox">
-        <img :src="profileData.userImage" alt="profile image" class="profile-image" v-if="profileData.userImage">
-        <img src="@/assets/images/profile.png" alt="profile image" class="profile-image" v-else>
+        <img
+          :src="profileData.userImage"
+          alt="profile image"
+          class="profile-image"
+          v-if="profileData.userImage"
+        />
+        <img src="@/assets/images/profile.png" alt="profile image" class="profile-image" v-else />
       </div>
       <div class="profile-content">
         <div class="profile-namebox">
           <div class="profile-name">{{ profileData.userNickname }}</div>
           <!-- v-if(나){프로필 편집} v-elseif(팔로우){팔로우취소} v-else -->
           <div class="profile-follow" v-if="!nickname">
-            <router-link :to="{ name: 'UpdateProfile', params: {profileData: profileData} }">프로필 편집</router-link>
+            <router-link :to="{ name: 'UpdateProfile', params: { profileData: profileData } }"
+              >프로필 편집</router-link
+            >
           </div>
 
-          <div class="profile-follow" v-if="(nickname && profileData.followState == 0 && followState == -1) || followState == 0" @click="follow">
+          <div
+            class="profile-follow"
+            v-if="
+              (nickname && profileData.followState == 0 && followState == -1) || followState == 0
+            "
+            @click="follow"
+          >
             <div>팔로우</div>
           </div>
-          
-          <div class="profile-follow" v-if="(nickname && profileData.followState == 1 && followState == -1) || followState == 1" @click="unfollow">
+
+          <div
+            class="profile-follow"
+            v-if="
+              (nickname && profileData.followState == 1 && followState == -1) || followState == 1
+            "
+            @click="unfollow"
+          >
             <div>언팔로우</div>
           </div>
 
-          <div class="profile-follow" v-if="(nickname && profileData.followState == 2 && followState == -1) || followState == 2">
+          <div
+            class="profile-follow"
+            v-if="
+              (nickname && profileData.followState == 2 && followState == -1) || followState == 2
+            "
+          >
             <div>신청중</div>
           </div>
-
         </div>
         <div class="profile-introduction">
           <span>{{ profileData.userComment }}</span>
@@ -32,55 +55,53 @@
       </div>
     </div>
     <div class="profile-bottom">
-      
-      <router-link :to="{ path: '/follow', query: {userNickname : profileData.userNickname}}">
+      <div>
+        <span class="tag">게시물</span>
+        <span>3</span>
+      </div>
+      <router-link :to="{ path: '/follow', query: { userNickname: profileData.userNickname } }">
         <div>
           <span class="tag">팔로잉</span>
           <span>{{ profileData.userFollowing }}</span>
         </div>
       </router-link>
-      <router-link :to="{ path: '/follow', query: {userNickname : profileData.userNickname}}">
+      <router-link :to="{ path: '/follow', query: { userNickname: profileData.userNickname } }">
         <div>
           <span class="tag">팔로워</span>
           <span>{{ profileData.userFollower }}</span>
         </div>
       </router-link>
-      <div>
-        <span class="tag">좋아요</span>
-        <span>806</span>
-      </div>
     </div>
   </div>
 </template>
 
 <script>
-import "@/components/css/profile/profileCard.css"
-import {requestFollow, unfollow} from "@/compositions/follow/follow"
+import "@/components/css/profile/profileCard.css";
+import { requestFollow, unfollow } from "@/compositions/follow/follow";
 export default {
-  name: 'ProfileCard',
+  name: "ProfileCard",
   props: {
     profileData: {
       type: Object,
     },
-    nickname:{
+    nickname: {
       type: String,
-    }
-  },
-  methods:{
-    follow(){
-      this.followState = 2;
-      requestFollow(this.profileData.userNickname)
     },
-    unfollow(){
-      this.followState = 0;
-      unfollow(this.profileData.userNickname)
-    }
   },
-  data: function(){
-    return{
-      followState : -1
-    }
-  }
-  
-}
+  methods: {
+    follow() {
+      this.followState = 2;
+      requestFollow(this.profileData.userNickname);
+    },
+    unfollow() {
+      this.followState = 0;
+      unfollow(this.profileData.userNickname);
+    },
+  },
+  data: function() {
+    return {
+      followState: -1,
+    };
+  },
+};
 </script>
