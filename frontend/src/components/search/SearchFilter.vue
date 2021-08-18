3cdafd73edc96f4863a6917c88a19f9e19a92820
jinho_pca <jinho_pca@naver.com>
2021-08-18 21:21:23 +0900

    fix: Update search page
    검색페이지 css 수정
    
    Resolved: S05P13A106-100

--- frontend/src/components/search/SearchFilter.vue
+++ frontend/src/components/search/SearchFilter.vue
@@ -1,77 +1,95 @@
 <template>
   <details open class="search-filter">
-    <summary><span class="filter-title">필터</span></summary>
-    <hr>
-    <div class="content">
-      <div class="filter-namebox">
-        <span class="filter-name" v-if="article==1" @click="clickArticle">게시물 종류</span>
-        <span class="filter-name active" v-else-if="article==2" @click="clickArticle">동선</span>
-        <span class="filter-name active" v-else-if="article==3" @click="clickArticle">장소</span>
+    <summary><span class="filter-title">검색조건</span></summary>
+    <hr />
+    <div class="content" style="text-align:center;">
+      <div class="filter-namebox" style="display:inline-block;">
+        <span class="filter-name" v-if="article == 1" @click="clickArticle">동선 + 장소</span>
+        <span class="filter-name active" v-else-if="article == 2" @click="clickArticle"
+          >동선 게시물</span
+        >
+        <span class="filter-name active" v-else-if="article == 3" @click="clickArticle"
+          >장소 게시물</span
+        >
       </div>
-      <div class="filter-namebox">
-        <span class="filter-name" v-if="comment==1" @click="clickComment">댓글</span>
-        <span class="filter-name active" v-else-if="comment==2" @click="clickComment">댓글↑</span>
-        <span class="filter-name active" v-else-if="comment==3" @click="clickComment">댓글↓</span>
+      &nbsp;&nbsp;&nbsp;
+      <div class="filter-namebox" style="display:inline-block;">
+        <span class="filter-name" v-if="like == 1" @click="clickLike">등록시간순</span>
+        <span class="filter-name active" v-else-if="like == 2" @click="clickLike"
+          >좋아요낮은순</span
+        >
+        <span class="filter-name active" v-else-if="like == 3" @click="clickLike"
+          >좋아요높은순</span
+        >
       </div>
-      <div class="filter-namebox">
-        <span class="filter-name" v-if="like==1" @click="clickLike">좋아요</span>
-        <span class="filter-name active" v-else-if="like==2" @click="clickLike">좋아요↑</span>
-        <span class="filter-name active" v-else-if="like==3" @click="clickLike">좋아요↓</span>
-      </div>
-      <div class="filter-date">
-        <div>
-          <label for="start">시작날짜</label>
+      <hr />
+      <div class="filter-date" style="text-align:center;">
+        <br />
+        <div style="display:inline-block">
+          <label for="start">start date</label>
           <div>
-            <input type="date" name="start" v-model="startDate">
+            <input type="date" name="start" v-model="startDate" />
           </div>
         </div>
-        <div>
-          <label for="end">끝날짜</label>
+        &nbsp;&nbsp;&nbsp;
+        <div style="display:inline-block;">
+          <label for="end">end date</label>
           <div>
-            <input type="date" name="end" v-model="endDate">
+            <input type="date" name="end" v-model="endDate" />
           </div>
         </div>
       </div>
     </div>
-    <div class="search-applybox">
+    <div class="search-applybox" style="text-align:center;">
+      <br />
       <button class="search-apply" @click="filterApply">적용</button>
     </div>
   </details>
 </template>
 
 <script>
-import "@/components/css/search/search-filter.css"
-import { ref } from 'vue'
+import "@/components/css/search/search-filter.css";
+import { ref } from "vue";
 
 export default {
-  name: 'SearchFilter',
+  name: "SearchFilter",
   setup(_, { emit }) {
-    const article = ref(1)
-    const comment = ref(1)
-    const like = ref(1)
-    const startDate = ref(new Date)
-    const endDate = ref(new Date)
+    const article = ref(1);
+    const comment = ref(1);
+    const like = ref(1);
+    const startDate = ref(new Date());
+    const endDate = ref(new Date());
 
     const clickArticle = () => {
-      article.value = article.value < 3 ? article.value+1 : 1
-    }
+      article.value = article.value < 3 ? article.value + 1 : 1;
+    };
     const clickComment = () => {
-      comment.value = comment.value < 3 ? comment.value+1 : 1
-    }
+      comment.value = comment.value < 3 ? comment.value + 1 : 1;
+    };
     const clickLike = () => {
-      like.value = like.value < 3 ? like.value+1 : 1
-    }
+      like.value = like.value < 3 ? like.value + 1 : 1;
+    };
     const filterApply = () => {
-      emit('getFilter', {
-        'article': article,
-        'comment': comment,
-        'like': like,
-        'startDate': startDate,
-        'endDate': endDate
-      })
-    }
+      emit("getFilter", {
+        article: article,
+        comment: comment,
+        like: like,
+        startDate: startDate,
+        endDate: endDate,
+      });
+    };
 
-    return { article, comment, like, startDate, endDate, clickArticle, clickComment, clickLike, filterApply }
-  }
-}
+    return {
+      article,
+      comment,
+      like,
+      startDate,
+      endDate,
+      clickArticle,
+      clickComment,
+      clickLike,
+      filterApply,
+    };
+  },
+};
 </script>
--- frontend/src/components/search/SearchRouteInput.vue
+++ frontend/src/components/search/SearchRouteInput.vue
@@ -1,50 +1,61 @@
 <template>
-  <div class="add-route-box">
-    <SearchPlace class="search-modal" v-if="showModal" @close="showModal = false" @place="addPlace"/>
+  <div class="add-route-box" style="border:1px solid rgba(68, 65, 65, 0.192); border-radius:15px;">
+    <div style="text-align:center;">검색 장소</div>
+    <br />
+    <SearchPlace
+      class="search-modal"
+      v-if="showModal"
+      @close="showModal = false"
+      @place="addPlace"
+    />
     <div class="added-route" v-for="(place, idx) in placeList" :key="idx" :place="place">
       <span class="added-place">{{ place.placeName }}</span>
       <span class="remove-route" @click="removeRoute">x</span>
     </div>
-		<button class="add-button" @click="showModal = true" v-if="placeList.length < 9">+</button>
+    <div>
+      <button class="add-button" @click="showModal = true" v-if="placeList.length < 9">
+        +
+      </button>
+    </div>
     <!-- <i class="fas fa-search" @click="getWord"></i> -->
   </div>
 </template>
 
 <script>
-import "@/components/css/search/search-route-input.css"
-import SearchPlace from '@/components/search/SearchPlace.vue'
-import { search } from '@/compositions/search.js'
+import "@/components/css/search/search-route-input.css";
+import SearchPlace from "@/components/search/SearchPlace.vue";
+import { search } from "@/compositions/search.js";
 
 export default {
-  name: 'SearchInput',
+  name: "SearchInput",
   components: {
-    SearchPlace
+    SearchPlace,
   },
   setup(_, { emit }) {
-    const { showModal, placeList, placePidList } = search()
+    const { showModal, placeList, placePidList } = search();
     async function addPlace(place) {
-			if(placePidList.value.includes(place.pid)){
-				alert('이미 추가한 장소 입니다.');
-				return
-			}
-      showModal.value = false
-      placePidList.value.push(place.pid)
-      placeList.value.push(place)
-      emit('routePid', placePidList)
-    }
-    
-    const removeRoute = () => {
-      const div = document.querySelectorAll('.added-route')
-      for (let i=0; i<div.length; i++) {
-        div[i].onclick = function() {
-          placeList.value.splice(i, 1)
-          placePidList.value.splice(i, 1)
-          emit('routePid', placePidList)
-        }
+      if (placePidList.value.includes(place.pid)) {
+        alert("이미 추가한 장소 입니다.");
+        return;
       }
+      showModal.value = false;
+      placePidList.value.push(place.pid);
+      placeList.value.push(place);
+      emit("routePid", placePidList);
     }
-    return { showModal, placeList, placePidList, addPlace, removeRoute }
+
+    const removeRoute = () => {
+      const div = document.querySelectorAll(".added-route");
+      for (let i = 0; i < div.length; i++) {
+        div[i].onclick = function() {
+          placeList.value.splice(i, 1);
+          placePidList.value.splice(i, 1);
+          emit("routePid", placePidList);
+        };
+      }
+    };
+    return { showModal, placeList, placePidList, addPlace, removeRoute };
   },
-  emits: ['routePid'],
-}
+  emits: ["routePid"],
+};
 </script>
