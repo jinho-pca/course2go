e09c390260e92b2623692e400e67edce79ab1a0f
jinho_pca <jinho_pca@naver.com>
2021-08-18 11:09:21 +0900

    fix: Update follow.vue
    팔로잉 표시 수정

--- frontend/src/views/profile/Follow.vue
+++ frontend/src/views/profile/Follow.vue
@@ -2,64 +2,67 @@
   <div class="follow">
     <div class="follow-tab">
       <i class="fas fa-chevron-left" @click="back"></i>
-      
+
       <!-- <FollowSearch /> -->
       <!-- <div class="follow-button">
         <button style="border: 1px solid black; box-shadow: 2px 2px 2px grey;">팔로잉</button>
         <button>팔로워</button>
       </div> -->
-      <FollowHeader @select="select"/>
+      <FollowHeader @select="select" />
     </div>
     <div v-show="!selectFollowing">
-      <FollowCard v-for="(userInfo,index) in followingListData" :key ="index" :userInfo="userInfo"/>
+      <FollowCard v-for="(userInfo, index) in followerListData" :key="index" :userInfo="userInfo" />
     </div>
     <div v-show="selectFollowing">
-      <FollowCard v-for="(userInfo,index) in followerListData" :key ="index" :userInfo="userInfo"/>
+      <FollowCard
+        v-for="(userInfo, index) in followingListData"
+        :key="index"
+        :userInfo="userInfo"
+      />
     </div>
-
   </div>
 </template>
 
 <script>
-import '@/assets/css/profile/follow.css';
+import "@/assets/css/profile/follow.css";
 // import FollowSearch from '@/components/profile/FollowSearch.vue'
-import FollowCard from '@/components/profile/FollowCard.vue'
-import FollowHeader from '@/components/profile/FollowHeader.vue'
-import {follow} from '@/compositions/profile/follow.js'
-import {useRoute} from 'vue-router'
+import FollowCard from "@/components/profile/FollowCard.vue";
+import FollowHeader from "@/components/profile/FollowHeader.vue";
+import { follow } from "@/compositions/profile/follow.js";
+import { useRoute } from "vue-router";
 
 export default {
-  name: 'Follow',
+  name: "Follow",
   components: {
     // FollowSearch,
     FollowCard,
-    FollowHeader
+    FollowHeader,
   },
-  
+
   setup() {
     const route = useRoute();
-    
+
     const back = () => {
       history.back();
-    }
-    const {followerListData, followerList, followingList, followingListData} = follow();
+    };
+    const { followerListData, followerList, followingList, followingListData } = follow();
     followerList(route.query.userNickname);
     followingList(route.query.userNickname);
-    return { back, followerListData, followerList, followingList, followingListData }
+    return { back, followerListData, followerList, followingList, followingListData };
   },
-  methods:{
-    select(select){
-      if(select == "팔로잉"){
+  methods: {
+    select(select) {
+      if (select == "팔로잉") {
         this.selectFollowing = true;
-      } else{
+      } else {
         this.selectFollowing = false;
       }
-    }
+    },
   },
-  data: function(){
-    return{
-      selectFollowing: true
-    }
-  }
-}
+  data: function() {
+    return {
+      selectFollowing: true,
+    };
+  },
+};
 </script>
