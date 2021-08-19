b74661b69771b0d9a1a3efc6447aad7f81411d0f
조규홍 <sitan516@gmail.com>
2021-08-20 01:49:39 +0900

    fix: Fix user delete
    팔로우가 연결되어 있으면 회원탈퇴 안되는 오류 수정

--- backend/src/main/java/com/course2go/service/user/UserDeleteServiceImpl.java
+++ backend/src/main/java/com/course2go/service/user/UserDeleteServiceImpl.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import com.course2go.dao.FollowDao;
 import com.course2go.dao.UserDao;
 import com.course2go.model.user.User;
 
@@ -14,7 +15,10 @@ public class UserDeleteServiceImpl implements UserDeleteService{
 
 	@Autowired
 	private UserDao userDao;
-
+	
+	@Autowired
+	private FollowDao followDao;
+	
 	@Autowired
 	BCryptPasswordEncoder passwordEncoder;
 	
@@ -24,7 +28,10 @@ public class UserDeleteServiceImpl implements UserDeleteService{
 		// 회원탈퇴 요청한 유저가 존재하는 경우
 		if(userDao.findUserByUserEmail(tokenEmail).isPresent()) {
 			Optional<User> result = userDao.findUserByUserEmail(tokenEmail);
-
+			String uid = result.get().getUid();
+			followDao.deleteAllByFollowFromUid(uid);
+			followDao.deleteAllByFollowToUid(uid);
+			
 			userDao.delete(result.get());
 			return 1;
 
